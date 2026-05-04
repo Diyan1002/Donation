fa1db10478754c6b319dd94d8079ed3748c22976
Muhammad Diyan Irfan <diyanirfan56@gmail.com>
2026-05-05 01:38:48 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/',  // Correct base path for deployment
-  build: {
-    outDir: 'dist',  // Ensure the output directory is correct
-  },
-});
+  plugins: [react()],
+})
